784bac8089bc9437bec60a7e6c5d106a2f024ab6
RavindarReddy7 <siddenki.ravindar@gmail.com>
2023-10-08 19:40:13 -0700

    checkout page

--- src/cart.js
+++ src/cart.js
@@ -20,22 +20,30 @@ let generatecartItems = () => {
       return (shoppingCart.innerHTML = basket
         .map((x) => {
             
-         let { id, item} = x;
+         let { id, item } = x;
          let search = shopItemsData.find((y) => y.id === id) || [];
+         let { img, name, price } = search;
         return `
        <div class="cart-item">
-         <img width="100" src=${search.img} alt = ""/>
+         <img width="100" src=${img} alt = ""/>
          <div class = "details">
           <div class = "title-price-x">
 
-          <h4>  <p>${search.name}</p>
+          <h4 class = "title-price">  
+             <p>${name}</p>
+             <p class = "cart-item-price" > $ ${price}</p>
+    
           </h4>
-          <i class="bi bi-x-lg"></i>
+          <i onclick="removeItem(${id})" class="bi bi-x-lg"></i>
            </div>
-
-          <div class = "cart-buttons"> </div>
-
-          <h3> </h3>
+            
+           <div class="buttons">
+                <i onclick="increment(${id})" class="bi bi-plus-square"></i>
+                <div id=${id} class="quantity">${item}
+                </div>
+                <i onclick="decrement(${id})" class="bi bi-dash-square"></i>
+           </div>
+            <h3>$ ${item * search.price}</h3>
           </div>
        </div>
         `;
@@ -54,3 +62,94 @@ let generatecartItems = () => {
 
 
 generatecartItems();
+
+let increment = (id) => {
+    let selectedItem = id;
+    let search = basket.find((x) => x.id === id);
+    console.log(selectedItem)
+    
+    if(search === undefined) {
+      basket.push({
+        id: id,
+        item: 1,
+    });
+  } else {
+    search.item +=1;
+  }
+  
+    generatecartItems();
+    update(selectedItem);
+    localStorage.setItem("data", JSON.stringify(basket));
+};
+
+let decrement = (id) => {
+    let selectedItem = id;
+
+    let search = basket.find((x) => x.id === id);
+
+    if(search === undefined) return;  
+    if(search.item === 0) return;
+    else{
+       search.item -= 1;
+    }
+
+    update(selectedItem);
+    basket = basket.filter((x) => x.item!==0);
+    generatecartItems();
+    localStorage.setItem("data", JSON.stringify(basket));
+};
+
+let update = (id) => {
+
+    let search = basket.find((x) => x.id === id);
+    //console.log(id)
+    document.getElementById(id).innerHTML = search.item;
+    calculation();
+    totalAmount();
+};
+
+
+let removetem = (id) => {
+    selectedItem = id;
+    basket = basket.filter((x)=> x.id !== id);
+    generatecartItems();
+    totalAmount();
+    calculation();
+    localStorage.setItem("data", JSON.stringify(basket));
+    
+}
+
+let totalAmount = () => {
+
+    if(basket.length!==0){
+        let amount = basket.map((x)=> {
+            let {id, item} = x;
+
+            let search = shopItemsData.find((y) => y.id === id) || [];
+
+            return item * search.price;
+
+        }).reduce((x,y)=>x+y,0);
+
+        label.innerHTML = `
+        <h2> Total Bill : $ ${amount} </h2>
+
+        <button class = "checkout">Checkout</button>
+        <button onclick = "clearCart()" class = "removeAll">Clear Cart</button>
+
+        `;
+
+    }
+    else
+    return
+
+}
+
+totalAmount();
+
+let clearCart = () => {
+    basket = [];
+    generatecartItems();
+    calculation();
+    localStorage.setItem("data", JSON.stringify(basket));
+};
