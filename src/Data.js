41d07ffde98c4970c88c2b1379a1b393100a6fb4
RavindarReddy7 <siddenki.ravindar@gmail.com>
2023-10-08 18:01:05 -0700

    home page

---
+++ src/Data.js
@@ -0,0 +1,28 @@
+let shopItemsData = [
+    {
+    id: "123",
+    name: "Casual Shirt",
+    price : 45,
+    desc : "lorem epsum",
+    img : "images/img-1.jpg",
+},
+{
+    id: "1234",
+    name: "T Shirt",
+    price : 15,
+    desc : "lorem epsum",
+    img : "images/img-2.jpg",
+},{
+    id: "12345",
+    name: "Suite",
+    price : 25,
+    desc : "lorem epsum",
+    img : "images/img-3.jpg",
+}, {
+    id: "123456",
+    name: "Track Pants",
+    price : 15,
+    desc : "lorem epsum",
+    img : "images/img-4.jpg",
+},
+];
---
+++ src/cart.js
@@ -0,0 +1,56 @@
+let label = document.getElementById("label");
+
+let shoppingCart = document.getElementById("shopping-cart");
+
+let basket = JSON.parse(localStorage.getItem("data")) || [];
+
+let calculation = () => {
+    let cartIcon = document.getElementById("cartAmount");
+
+    cartIcon.innerHTML = basket.map((x) => x.item).reduce((x,y) => x + y, 0);
+};
+
+calculation();
+
+
+let generatecartItems = () => {
+
+    if (basket.length !==0){
+      
+      return (shoppingCart.innerHTML = basket
+        .map((x) => {
+            
+         let { id, item} = x;
+         let search = shopItemsData.find((y) => y.id === id) || [];
+        return `
+       <div class="cart-item">
+         <img width="100" src=${search.img} alt = ""/>
+         <div class = "details">
+          <div class = "title-price-x">
+
+          <h4>  <p>${search.name}</p>
+          </h4>
+          <i class="bi bi-x-lg"></i>
+           </div>
+
+          <div class = "cart-buttons"> </div>
+
+          <h3> </h3>
+          </div>
+       </div>
+        `;
+        })
+        .join(""));
+    } else {
+      shoppingCart.innerHTML =``;
+      label.innerHTML = `
+      <h2>Cart is Empty </h2>
+      <a href = "index.html">
+        <button class = "HomeBtn">Back to home</button>
+    </a>
+    `;
+    }
+};
+
+
+generatecartItems();
